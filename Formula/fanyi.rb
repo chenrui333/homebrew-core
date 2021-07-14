e58fc7d19a1e17b94c12d343985bbbb82b7a287d
Rui Chen <rui@chenrui.dev>
2021-07-13 21:42:53 -0400

    fanyi 5.0.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/fanyi.rb
@@ -0,0 +1,20 @@
+require "language/node"
+
+class Fanyi < Formula
+  desc "🇨🇳 and 🇺🇸 translate tool in your command-line"
+  homepage "https://github.com/afc163/fanyi"
+  url "https://registry.npmjs.org/fanyi/-/fanyi-5.0.1.tgz"
+  sha256 "0f80edeac5e727a299c2a0807f463231143d59b46fd8308ef794390b9d88052c"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "爱", shell_output("#{bin}/fanyi lov 2>/dev/null")
+  end
+end
