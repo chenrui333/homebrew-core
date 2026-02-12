1a9e06192bf17b445d88a9262de1ffc54691a1e7
Rui Chen <rui@chenrui.dev>
2026-02-12 07:03:36 -0500

    bun: prototype rewrite ThrowScope include

--- Formula/b/bun.rb
+++ Formula/b/bun.rb
@@ -447,6 +447,9 @@ class Bun < Formula
     inreplace "src/bun.js/bindings/JSCInlines.h",
               "#include <JavaScriptCore/StructureInlines.h>",
               "#include <StructureInlines.h>"
+    inreplace "src/bun.js/bindings/JSCInlines.h",
+              "#include <JavaScriptCore/ThrowScope.h>",
+              "#include <ThrowScope.h>"
     webkit_download_block = <<~CMAKE
       file(
         DOWNLOAD ${WEBKIT_DOWNLOAD_URL} ${CACHE_PATH}/${WEBKIT_FILENAME} SHOW_PROGRESS
