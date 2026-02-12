3c13126d6d4962b6b00c42c3f7dab7a477a49df8
Rui Chen <rui@chenrui.dev>
2026-02-12 06:44:25 -0500

    bun: prototype slotvisitor include mapping

--- Formula/b/bun.rb
+++ Formula/b/bun.rb
@@ -438,6 +438,9 @@ class Bun < Formula
     inreplace "src/bun.js/bindings/JSCInlines.h",
               "#include <JavaScriptCore/Operations.h>",
               "#include <Operations.h>"
+    inreplace "src/bun.js/bindings/JSCInlines.h",
+              "#include <JavaScriptCore/SlotVisitorInlines.h>",
+              "#include <SlotVisitorInlines.h>"
     webkit_download_block = <<~CMAKE
       file(
         DOWNLOAD ${WEBKIT_DOWNLOAD_URL} ${CACHE_PATH}/${WEBKIT_FILENAME} SHOW_PROGRESS
