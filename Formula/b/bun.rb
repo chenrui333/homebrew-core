7e81689fd2767495a6d9e8111c8e0807d8fdbb83
Rui Chen <rui@chenrui.dev>
2026-02-12 01:26:59 -0500

    bun: prototype rewrite HeapInlines include

--- Formula/b/bun.rb
+++ Formula/b/bun.rb
@@ -405,6 +405,9 @@ class Bun < Formula
     inreplace "src/bun.js/bindings/JSCInlines.h",
               "#include <JavaScriptCore/GCIncomingRefCountedInlines.h>",
               "#include <GCIncomingRefCountedInlines.h>"
+    inreplace "src/bun.js/bindings/JSCInlines.h",
+              "#include <JavaScriptCore/HeapInlines.h>",
+              "#include <HeapInlines.h>"
     webkit_download_block = <<~CMAKE
       file(
         DOWNLOAD ${WEBKIT_DOWNLOAD_URL} ${CACHE_PATH}/${WEBKIT_FILENAME} SHOW_PROGRESS
