33805c81091d2925ca7b05dea714bc75a8dd06d8
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:46 -0400

    draft: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/draft.rb
+++ Formula/d/draft.rb
@@ -18,6 +18,12 @@ class Draft < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ldflags = "-X github.com/Azure/draft/cmd.VERSION=#{version}"
     system "go", "build", *std_go_args(ldflags:)
