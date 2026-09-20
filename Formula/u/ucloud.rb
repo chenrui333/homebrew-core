2379c8bac33c62ae81e398f89c4607292d967d7d
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:50 -0400

    ucloud: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/ucloud.rb
+++ Formula/u/ucloud.rb
@@ -17,6 +17,12 @@ class Ucloud < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ldflags = "-X github.com/ucloud/ucloud-cli/cmd/internal/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
