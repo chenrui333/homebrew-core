31fd603c5f0cb29126f15fc17271d4a6e50553df
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:51 -0400

    wayback: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wayback.rb
+++ Formula/w/wayback.rb
@@ -22,6 +22,12 @@ class Wayback < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ldflags = %W[
       -X github.com/wabarc/wayback/version.Version=#{version}
