2ffa7c89bfd4235af5cabd0f42f6021568081488
Rui Chen <rui@chenrui.dev>
2026-09-20 05:34:00 -0400

    tty-share: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tty-share.rb
+++ Formula/t/tty-share.rb
@@ -19,6 +19,8 @@ class TtyShare < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.version=#{version}")
   end
