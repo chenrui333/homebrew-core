3bbb733e645f291a3a1bb63cf9428f5c3a13a58b
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:45 -0400

    argocd-vault-plugin: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/argocd-vault-plugin.rb
+++ Formula/a/argocd-vault-plugin.rb
@@ -19,6 +19,12 @@ class ArgocdVaultPlugin < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ENV["CGO_ENABLED"] = "0"
 
