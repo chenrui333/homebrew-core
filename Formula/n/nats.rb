491d47f4abe6d48eab15c3e0e4071dbaba9febb5
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:48 -0400

    nats: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nats.rb
+++ Formula/n/nats.rb
@@ -16,6 +16,12 @@ class Nats < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     system "go", "build", *std_go_args(ldflags: "-X main.version=v#{version}"), "./nats"
     generate_completions_from_executable(bin/"nats", shells:                 [:bash, :zsh],
