5af9d0d9ff1de56c8a8bc220bc936be3c799f144
Rui Chen <rui@chenrui.dev>
2026-09-20 06:00:47 -0400

    hishtory: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/hishtory.rb
+++ Formula/h/hishtory.rb
@@ -24,6 +24,12 @@ class Hishtory < Formula
 
   depends_on "go" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "go", "mod", "download"
+  end
+
   def install
     ldflags = %W[
       -X github.com/ddworken/hishtory/client/lib.Version=#{version}
