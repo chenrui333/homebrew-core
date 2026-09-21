b9c5483b9190a5710cfc5fdb6c2bd4c06e998db3
Rui Chen <rui@chenrui.dev>
2026-09-21 00:22:49 -0400

    hexyl: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/hexyl.rb
+++ Formula/h/hexyl.rb
@@ -19,6 +19,12 @@ class Hexyl < Formula
   depends_on "pandoc" => :build
   depends_on "rust" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
     system "pandoc", "-s", "-f", "markdown", "-t", "man",
