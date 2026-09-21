df17a376dd51ba19f249c0a85b6162b98747c918
Rui Chen <rui@chenrui.dev>
2026-09-21 00:14:29 -0400

    angle-grinder: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/angle-grinder.rb
+++ Formula/a/angle-grinder.rb
@@ -19,6 +19,12 @@ class AngleGrinder < Formula
 
   depends_on "rust" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
