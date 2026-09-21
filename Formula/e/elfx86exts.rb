c8b9628aa51196b615e9f1d722c78a51db1fe006
Rui Chen <rui@chenrui.dev>
2026-09-21 00:45:21 -0400

    elfx86exts: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/elfx86exts.rb
+++ Formula/e/elfx86exts.rb
@@ -28,6 +28,12 @@ class Elfx86exts < Formula
   depends_on "rust" => :build
   depends_on "capstone"
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
