e48756b4a7f803d3e853900558acfbcf33b65e05
Rui Chen <rui@chenrui.dev>
2026-09-21 01:35:33 -0400

    hck: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/hck.rb
+++ Formula/h/hck.rb
@@ -28,6 +28,12 @@ class Hck < Formula
     depends_on "zlib-ng-compat"
   end
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
