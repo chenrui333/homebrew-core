43fc760c3eddc2d322228bbdd4078eb38f561166
Rui Chen <rui@chenrui.dev>
2026-09-21 01:14:50 -0400

    ghcitty: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/ghcitty.rb
+++ Formula/g/ghcitty.rb
@@ -18,6 +18,12 @@ class Ghcitty < Formula
   depends_on "rust" => :build
   depends_on "ghc" => :test
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
