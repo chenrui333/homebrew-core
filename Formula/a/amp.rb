1b8904f957fbf584a8a8deeade10f2c0ee8ca904
Rui Chen <rui@chenrui.dev>
2026-09-21 00:29:19 -0400

    amp: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/amp.rb
+++ Formula/a/amp.rb
@@ -27,6 +27,12 @@ class Amp < Formula
     depends_on "zlib-ng-compat"
   end
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
