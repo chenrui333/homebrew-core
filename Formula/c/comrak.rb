8fd3693a221743f4a4ace66c0977b136c4b34bca
Rui Chen <rui@chenrui.dev>
2026-09-20 23:52:41 -0400

    comrak: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/comrak.rb
+++ Formula/c/comrak.rb
@@ -17,6 +17,12 @@ class Comrak < Formula
 
   depends_on "rust" => :build
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
