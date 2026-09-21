212adc0111a54814dcb7249847ab70e18d57cd57
Rui Chen <rui@chenrui.dev>
2026-09-21 01:20:17 -0400

    gpg-tui: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gpg-tui.rb
+++ Formula/g/gpg-tui.rb
@@ -22,6 +22,12 @@ class GpgTui < Formula
   depends_on "libgpg-error"
   depends_on "libxcb"
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
 
