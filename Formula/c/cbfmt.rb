2bd77b89659fb8aa2907eeeb65a19f01223ea105
Rui Chen <rui@chenrui.dev>
2026-09-21 00:33:22 -0400

    cbfmt: add deny_network_access!
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cbfmt.rb
+++ Formula/c/cbfmt.rb
@@ -19,6 +19,12 @@ class Cbfmt < Formula
 
   depends_on "rust" => [:build, :test]
 
+  deny_network_access!
+
+  def fetch
+    system "cargo", "fetch", "--locked", "--target", "host-tuple"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
