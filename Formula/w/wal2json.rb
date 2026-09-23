e949ccd9051e80ebbe413571f6882e5c3b1b476b
Rui Chen <rui@chenrui.dev>
2026-09-23 09:37:53 -0400

    wal2json: allow test network access
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/w/wal2json.rb
+++ Formula/w/wal2json.rb
@@ -31,6 +31,8 @@ class Wal2json < Formula
     deps.map(&:to_formula).sort_by(&:version).filter { |f| f.name.start_with?("postgresql@") }
   end
 
+  allow_network_access! :test
+
   def install
     odie "Too many postgresql dependencies!" if postgresqls.count > 2
 
